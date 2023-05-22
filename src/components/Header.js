c723f9c6505327c14e586477b1b17044002c0ec4
notlikethiss <notlikeethiss@gmail.com>
2023-05-22 14:54:15 +0300

    adding icons to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,15 +25,23 @@ const Header = ({selected = ''}) => {
 
             <div className="header">
                 <div className="header_left">
-                    <Link to="/" className={selected === 'home' ? 'active' : ''}>Главная</Link>
-                    <Link to="/last" className={selected === 'last_releases' ? 'active' : ''}>Последние релизы</Link>
+                    {/* Заменить src на путь с сервера */}
+                    <div className="header_link">
+                        <img className={selected === 'home' ? 'selected_icon' : 'header_icon'} alt='icon' src='https://media.discordapp.net/attachments/888505536457367552/1109937237472313375/icons8-home-48.png'></img>
+                        <Link to="/" className={selected === 'home' ? 'active' : ''}>Главная</Link>
+                    </div>
+                    <div className="header_link">
+                        <img className={selected === 'last_releases' ? 'selected_icon' : 'header_icon'} alt='icon' src='https://media.discordapp.net/attachments/888505536457367552/1109939390693769257/icons8-collage-60.png'></img>
+                        <Link to="/last" className={selected === 'last_releases' ? 'active' : ''}>Последние релизы</Link>
+                    </div>
                     {/*<Link to="/random" className={selected === 'random_release' ? 'active no1000' : 'no1000'}>Случайный релиз</Link>*/}
-                    <Link to="/app" className={selected === 'app' ? 'active no1000' : 'no1000'}>Приложение</Link>
-
+                    <div className="header_link">
+                        <img className={selected === 'app' ? 'selected_icon' : 'header_icon'} alt='icon' src='https://media.discordapp.net/attachments/888505536457367552/1109932067480940544/icons8-download-48.png'></img>
+                        <Link to="/app" className={selected === 'app' ? 'active no1000' : 'no1000'}>Приложение</Link>
+                    </div>
                     <Link to="/search" className={selected === 'search' ? 'active header_mob_search' : 'header_mob_search'}>Поиск</Link>
 
                     <HeaderSearch/>
-
 
                 </div>
                 <div className="header_right">
