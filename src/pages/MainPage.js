3f3ab57cba0ec68acf7e37c2ceae4ac487583c1e
fllcker <jxwide@gmail.com>
2023-05-27 19:00:01 +0300

    added warning

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from "../components/Header";
-import { useNavigate } from "react-router-dom";
-import ParticlesComp from "../components/ParticlesComp";
+import {Link, useNavigate} from "react-router-dom";
 import { useCookies } from 'react-cookie';
 
 const MainPage = () => {
@@ -13,28 +12,23 @@ const MainPage = () => {
     return (
         <>
 
-            <ParticlesComp />
-
             <Header selected='home' />
 
             <div className="page">
                 <div className="page_content">
-                    <h1 className="main_page_title">Смотри аниме на
-                        <span className="main_page_title_green"> AniType</span>
-                    </h1>
-                    <p className="main_page_desc">Огромное количество релизов с озвучкой от AniLibria, современный дизайн и не только, зарегистрируйся прямо сейчас</p>
+                    <h1 className="main_page_title">Что случилось?</h1>
+                    <p className="main_page_desc">Мы решили полностью переделать сайт. Поэтому того, что видите сейчас скоро не будет. Новый дизайн функционал вы можете посмотреть в моем тиктоке. Возможно, то, что есть сейчас останется на другом домене, и с урезанным функционалом. В любом случае, то, что будет вас точно удивит! Следите за новостями в нашем дискорд сервере и телеграм канале.</p>
 
-                    {
-                        cookies.username ? (
-                            <div className="go_reg">
-                                <button onClick={() => nav('/last')}>Перейти к просмотру</button>
-                            </div>
-                        ) : (
-                            <div className="go_reg">
-                                <button onClick={() => nav('/auth/signup')}>Зарегистрироваться</button>
-                            </div>
-                        )
-                    }
+
+
+                    <Link to="https://t.me/anitypenews">
+                        <button>t.me/anitypenews</button>
+                    </Link>
+                    <p></p>
+                    <iframe src="https://discord.com/widget?id=1109422526762864682&theme=dark" width="350" height="500"
+                            allowTransparency="true" frameBorder="0"
+                            sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+
                 </div>
             </div>
             <video src="https://cdn.discordapp.com/attachments/1109427599379267655/1109925819775725729/pidor.webm" className="backgroundvideo" muted autoPlay loop playsInline></video>
